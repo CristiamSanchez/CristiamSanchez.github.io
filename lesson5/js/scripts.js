9b3141d6c6984c32764762655d5499c00f371e33
Cristiam <Cristiam@Cristians-MacBook.local>
2020-10-16 18:32:26 -0600

    lesson5

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+function toggleMenu(){   
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+const daysname = ['Sunday', 'Thursday', 'Wednesday', 'Tuesday', 'Friday', 'Saturday', 'Monday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const todaysdate = new Date();
+const dayName = daysname[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const year = todaysdate.getFullYear;
+const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+
+document.getElementById('currentdate').textContent = currentdate;
+
+
+
+
+
