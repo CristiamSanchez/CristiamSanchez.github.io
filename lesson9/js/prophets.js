8a8de8a32302dd7ed8cc614b43d1da2eb63ec1ed
Cristiam <Cristiam@Cristians-MacBook.local>
2020-11-11 20:45:07 -0600

    prophets1.1 Showing all

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,7 +5,7 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
+        //console.table(jsonObject); // temporary checking for valid response and data parsing
 
         const prophets = jsonObject['prophets'];
 
@@ -13,13 +13,28 @@ fetch(requestURL)
 
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
+        let bd = document.createElement('h3');
+        let bp = document.createElement('h3');
+        let order = document.createElement('h3');
+        let imagen = document.createElement('img');
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        order.textContent = 'Order: ' + prophets[i].order; 
+        bd.textContent = 'Birthday: ' + prophets[i].birthdate;
+        bp.textContent = 'Bith Place: ' + prophets[i].birthplace; 
+        imagen.image =  image.setAttribute('src', prophets[i].imageurl);    
+
 
         card.appendChild(h2);
-
+        card.appendChild(order);
+        card.appendChild(bd);
+        card.appendChild(bp);
+        card.appendChild(imagen);
         document.querySelector('div.cards').appendChild(card);
-        image.setAttribute('src', prophets[i].imageurl);
         }
+        
+        //image.setAttribute('src', prophets[i].imageurl);
     });
+
+        
 
