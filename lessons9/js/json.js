1fdbdccc9b60f50ebfda152f009f8acc3fb03f91
Cristiam <Cristiam@Cristians-MacBook.local>
2020-11-18 11:53:30 -0600

    Lesson9 changes Json

--- lessons9/js/json.js
+++ lessons9/js/json.js
@@ -6,7 +6,7 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         const towns = jsonObject['towns'];
-        for (let i = 0; i < towns.length; i++ ) {
+        for (let i = 0; i < towns.length; i++) {
             let card = document.createElement('section');
             let div = document.createElement('div');
             let name = document.createElement('h2');
@@ -15,31 +15,32 @@ fetch(requestURL)
             let rainfall = document.createElement('p');
             let img = document.createElement('img');
 
-            name.textContent = " "+ towns[i].name + " ";
-            year.textContent = "Year Founded: " + towns[i].yearFounded; 
+            name.textContent = " " + towns[i].name + " ";
+            year.textContent = "Year Founded: " + towns[i].yearFounded;
             population.textContent = "Population: " + towns[i].currentPopulation;
             rainfall.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-            if(towns[i].name == "Preston"){
+            if (towns[i].name == "Preston") {
                 img.setAttribute('src', "images/prestono.jpg");
                 img.setAttribute('alt', "Photograph of: " + name.textContent);
-            }else if(towns[i].name == "Springfield"){
+            } else if (towns[i].name == "Springfield") {
                 img.setAttribute('src', "images/fisho.jpg");
                 img.setAttribute('alt', "Photograph of: " + name.textContent);
-            }else if(towns[i].name == "Placerton"){
+            } else if (towns[i].name == "Placerton") {
                 img.setAttribute('src', "images/sodaso.jpg");
                 img.setAttribute('alt', "Photograph of: " + name.textContent);
-            }else{}
+            } else {}
             //img.setAttribute('src', "images/" + towns[i].photo);
             //img.setAttribute('alt', "Photograph of: " + name.textContent);
 
-            if (i==0 || i==4 || i==5){
-            div.appendChild(name);
-            div.appendChild(year);
-            div.appendChild(population);
-            div.appendChild(rainfall);
-            card.appendChild(div);
-            card.appendChild(img);
+            if (i == 0 || i == 4 || i == 5) {
+                div.appendChild(name);
+                div.appendChild(year);
+                div.appendChild(population);
+                div.appendChild(rainfall);
+                card.appendChild(div);
+                card.appendChild(img);
 
-            document.querySelector('div.cities').appendChild(card);}
-    }
-  });
+                document.querySelector('div.cities').appendChild(card);
+            }
+        }
+    });
